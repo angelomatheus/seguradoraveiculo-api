6334c8f9d378bb39bc60cd2fd0571132c2bd23a5
AngeloMatheus <angelo.sampaio.silva@gmail.com>
2021-06-14 16:30:58 -0400

    commit inicial para vincular o repositorio

---
+++ src/main/java/com/angelosampaio/visionnaire/seguradoraveiculos/DesafiovisionnaireseguradoraApplication.java
@@ -0,0 +1,13 @@
+package com.angelosampaio.visionnaire.seguradoraveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiovisionnaireseguradoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiovisionnaireseguradoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angelosampaio/visionnaire/seguradoraveiculos/DesafiovisionnaireseguradoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelosampaio.visionnaire.seguradoraveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiovisionnaireseguradoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
